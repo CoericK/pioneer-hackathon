10b660151b59aa0166889de55b7412c5bcb30107
azlen <azlen.elz@gmail.com>
2020-04-11 17:34:51 -0700

    add dist build

---
+++ dist/precache-manifest.84b7a5130cdafb1e930c722bd9a3c295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "afa5376e76a9517e68541ec54f29ff71",
+    "url": "/style.css"
+  },
+  {
+    "revision": "7130fc95195b3fa12d5f",
+    "url": "/js/chunk-vendors.568fa1c2.js"
+  },
+  {
+    "revision": "9c102ca01f0991797d50",
+    "url": "/js/app.171ddcb5.js"
+  },
+  {
+    "revision": "37d4688ea754ab38fea5e277dec31481",
+    "url": "/index.html"
+  },
+  {
+    "revision": "88e2a938d92d344dcc3ca252f652e2ac",
+    "url": "/img/room.88e2a938.png"
+  },
+  {
+    "revision": "9c102ca01f0991797d50",
+    "url": "/css/app.d55df6ca.css"
+  }
+];
